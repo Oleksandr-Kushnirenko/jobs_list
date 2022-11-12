e505e2b19d863ac93590a389273e0128aa2f3210
Oleksandr Kushnirenko <kushnirenko.oleksandr@gmail.com>
2022-11-12 12:20:01 +0300

    start request

--- src/services/JobService.js
+++ src/services/JobService.js
@@ -1,1 +1,13 @@
-import {useHttp} from ".."
+import {useHttp} from "../hooks/http.hooks";
+
+const useJobService = () => {
+    const {request, clearError, process, setProcess} = useHttp();
+
+    const _apiBase = "";
+    const _apiKey = "";
+    const _baseOffset = 0;
+
+    const getAllJobs = async (offset = _baseOffset) => {
+        const res = await request(`{_apiBase}`)
+    }
+}
